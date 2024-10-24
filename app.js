17e065e1a457e023ac8e08d57b62d15fba1c5fc8
zaffaelma <zaffazidnielma@gmail.com>
2024-10-24 13:55:36 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+  const tinggiBadan = ["150", "155", "148", "165", "170", "168", "166"]
+  const namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
 
-
+  for(let i=0; i < namaAtlet.length; i++){
+    console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]}`)
+  }
 
 
 
 
   /// JANGAN UBAH SELAINNYA
 }
-
+runner()
 module.exports = runner
